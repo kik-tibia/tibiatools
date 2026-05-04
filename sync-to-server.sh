a4222147e4c4cf6eeb6bfcf4430dec74748d93bf
Kikaro <kik-tibia@users.noreply.github.com>
2026-05-04 08:51:14 +0100

    version.txt

--- sync-to-server.sh
+++ sync-to-server.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+git -C "$(dirname "$0")" rev-parse HEAD > "$(dirname "$0")/public/version.txt"
 rsync -avz --delete --exclude .git --exclude node_modules --exclude dist "$(dirname "$0")/" tibiatools:/var/www/tibiatools/
